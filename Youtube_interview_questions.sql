5a5fa1dccc94d40c25d4a4fe7373b09cec01c4b8
Nilesh-kodag <kodagnilesh@gmail.com>
2025-10-23 14:29:08 +0530

    additing more question and answer

--- Youtube_interview_questions.sql
+++ Youtube_interview_questions.sql
@@ -1,3 +1,24 @@
 -- ankit bandsal interview questions
 
--- Quetion 1 
+-- Quetion 1
+-- Complex SQL 2 | find new and repeat customers  
+
+Use test_nilesh_learning;
+
+
+-- with first_intraction as (
+-- select customer_id, min(order_date) as first_intraction from customer_orders
+-- Group by customer_id)
+
+
+-- SELECT co.order_date,
+-- sum(Case when co.order_date=fi.first_intraction then 1 else 0 end) as  New_customer,
+-- sum( case when co.order_date!=fi.first_intraction then 1 else 0 end) as Repetative_customer
+--  FROM customer_orders as co
+-- Left Join first_intraction as fi on co.customer_id=fi.customer_id
+-- Group by co.order_date;
+
+
+
+-- Question 2 
+-- Derive Points table for ICC tournament
